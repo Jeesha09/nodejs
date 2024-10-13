1d25c332cd827bf2dfeec70449ebd28ce8110a91
Jeesha09 <jsavla789@gmail.com>
2024-10-14 00:30:40 +0530

    Adding the code

---
+++ 5-utils.js
@@ -0,0 +1,9 @@
+// Modules - Encapsulaed Code (only share minimum)
+// CommonJS, every file is module (by default)
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
+// export default
+module.exports = sayHi
