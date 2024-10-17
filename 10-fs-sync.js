b6449445659c62b797ff0749bd9ab3756cd5da2e
Jeesha09 <jsavla789@gmail.com>
2024-10-17 21:40:54 +0530

    first commit

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,10 +1,10 @@
 const { readFileSync, writeFileSync } = require('fs')
 console.log('start')
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
+const first = readFileSync('./content/subfolder/first.txt', 'utf8')
+const second = readFileSync('./content/subfolder/second.txt', 'utf8')
 
 writeFileSync(
-  './content/result-sync.txt',
+  './content/subfolder/result-sync.txt',
   `Here is the result : ${first}, ${second}`,
   { flag: 'a' }
 )
--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,20 +1,20 @@
 const { readFile, writeFile } = require('fs')
 
 console.log('start')
-readFile('./content/first.txt', 'utf8', (err, result) => {
+readFile('./content/subfolder/first.txt', 'utf8', (err, result) => {
   if (err) {
     console.log(err)
     return
   }
   const first = result
-  readFile('./content/second.txt', 'utf8', (err, result) => {
+  readFile('./content/subfolder/second.txt', 'utf8', (err, result) => {
     if (err) {
       console.log(err)
       return
     }
     const second = result
     writeFile(
-      './content/result-async.txt',
+      './content/subfolder/result-async.txt',
       `Here is the result : ${first}, ${second}`,
       (err, result) => {
         if (err) {
