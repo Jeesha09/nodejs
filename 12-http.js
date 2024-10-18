534cdc0bd9d91ed00ac80299cedcaff09c0928d2
Jeesha09 <jsavla789@gmail.com>
2024-10-19 00:55:43 +0530

    next 2 hours

--- 12-http.js
+++ 12-http.js
@@ -12,8 +12,8 @@ const server = http.createServer((req, res) => {
   // <p>We can't seem to find the page you are looking for</p>
   // <a href="/">back home</a>
   //   `)
-  // ###################################
-  // ###################################
+  // #####
+  // #####
   //
   //  IF YOU GET ERRORS WHILE USING ABOVE SETUP,
   // SWITCH TO IF, ELSE IF, ELSE (BELOW)
--- app.js
+++ app.js
@@ -18,9 +18,7 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
 
+// npm uninstall packageName
+  // npm i nodemon -D
 
-const _ = require('lodash')
-
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+co
